3cdf67b8c90a1aff5cc4e170c3fac462516d59d8
Kotelan <ellswjad000@alpinesd.org>
2024-10-22 11:16:01 -0600

    for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,8 @@
 #for Summation code here
+num = int(input("Enter a positive number: "))
+loops = 0
+sum = 0
+for loops in range(num):
+    loops = loops +1
+    sum = sum + loops
+print("The summation of " + str(num) + " is " + str(sum))
